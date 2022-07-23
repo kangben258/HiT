8791fdd8bb33816d582c34006ac05e60cc7773bf
kangben258 <1969868860@qq.com>
2022-07-23 09:34:48 +0800

    maxf commit

--- lib/train/admin/local.py
+++ lib/train/admin/local.py
@@ -1,16 +1,16 @@
 class EnvironmentSettings:
     def __init__(self):
-        self.workspace_dir = '/home/kb/EdgeTrack-main'    # Base directory for saving network checkpoints.
-        self.tensorboard_dir = '/home/kb/EdgeTrack-main/tensorboard'    # Directory for tensorboard files.
-        self.pretrained_networks = '/home/kb/EdgeTrack-main/pretrained_networks'
+        self.workspace_dir = '/root/EdgeTrack'    # Base directory for saving network checkpoints.
+        self.tensorboard_dir = '/root/EdgeTrack-main/tensorboard'    # Directory for tensorboard files.
+        self.pretrained_networks = 'EdgeTrack-main/pretrained_networks'
         self.lasot_dir = '/home/kb/EdgeTrack-main/data/LaSOTBenchmark'
         self.got10k_dir = '/home/sp3090/kangben/EdgeTrack-main/data/got10k'
-        self.lasot_lmdb_dir = '/home/kb/EdgeTrack-main/data/lasot_lmdb'
-        self.got10k_lmdb_dir = '/home/kb/EdgeTrack-main/data/got10k_lmdb'
-        self.trackingnet_dir = '/home/sp3090/kangben/EdgeTrack-main/data/trackingnet'
-        self.trackingnet_lmdb_dir = '/home/kb/EdgeTrack-main/data/trackingnet_lmdb'
+        self.lasot_lmdb_dir = '/root//autodl-extra/data/tracking_data/lasot_lmdb'
+        self.got10k_lmdb_dir = '/root/autodl-extra/data/tracking_data/got10k_lmdb'
+        self.trackingnet_dir = '/root/autodl-extra/data/tracking_data/trackingnet_lmdb'
+        self.trackingnet_lmdb_dir = '/root/autodl-extra/data/tracking_data/trackingnet_lmdb'
         self.coco_dir = '/home/sp3090/kangben/EdgeTrack-main/data/coco'
-        self.coco_lmdb_dir = '/home/kb/EdgeTrack-main/data/coco_lmdb'
+        self.coco_lmdb_dir = '/root/autodl-extra/data/tracking_data/coco_lmdb'
         self.imagenet1k_dir = '/home/kb/EdgeTrack-main/data/imagenet1k'
         self.lvis_dir = ''
         self.sbd_dir = ''
